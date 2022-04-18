fdbd58616c0ae056de790587849f65a57eb6f218
Maureen-Muriithi <moh2wanja@gmail.com>
2022-04-18 18:11:00 +0300

    Create the constructor and interface for the API reaponse

--- src/app/Classes/repository.ts
+++ src/app/Classes/repository.ts
@@ -1,3 +1,3 @@
 export class Repository {
-    constructor(public name: string, public full_name: string, public description: string, public updated_at:Date, public html_url: string, public language: string, public watching: number, public created_at:Date){}
+    constructor(public name: string, public full_name: string, public description: string, public html_url: string, public language: string, public watching: number, public updated_at:Date, public created_at:Date){}
 }
--- src/app/Services/search-github.service.ts
+++ src/app/Services/search-github.service.ts
@@ -1,10 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../Classes/user';
+import { Repository } from '../Classes/repository';
+import { environment } from 'src/environments/environment';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
 })
 export class SearchGithubService {
 
-  constructor() { }
+  user!:User;
+  repos!:Repository;
+
+  constructor(private http:HttpClient) { 
+    this.user = new User("",0, "", 0,0,"",new Date(),new Date());
+    this.repos = new Repository("","","","","",0,new Date(),new Date());
+  }
+
+  getUserData(username: string){
+    interface ApiResponse{
+      name:string,
+	    public_repos:number,
+	    login:string,
+      followers:number
+      following:number
+	    avatar_url:string,
+	    created_at:Date, 
+	    updated_at:Date, 
+	    html_url:string   
+  }
+
+}
 }
